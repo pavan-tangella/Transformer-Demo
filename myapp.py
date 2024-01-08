a568dba46ba55dfe09ef3277edbe998ee59a142c
pavan-tangella <136261705+pavan-tangella@users.noreply.github.com>
2024-01-08 12:00:46 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,12 +1,12 @@
 import streamlit as st
 st.set_page_config(page_title="Kings in flim Industry")
-st.header("Types of HEROS in Film industry")
+st.header("HEROS in Film industry")
 col1,col2=st.columns(2)
 with col1:
   st.subheader("Prabhas")
-  st.image("prabhas.jpg",caption="Prabhas",width=300)
+  st.image("prabhas.jpg",caption="Prabhas",width=300,height=300)
   st.write("Image from SALAAR")
 with col2:
   st.subheader("Yesh")
-  st.image("Rocky.jpg",caption="YESH",width=300)
+  st.image("Rocky.jpg",caption="YESH",width=300,height=300)
   st.write("Image from KGF")
