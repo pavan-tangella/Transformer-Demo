ec737d58c4973e86c5b786a0a41821927798c041
pavan-tangella <136261705+pavan-tangella@users.noreply.github.com>
2024-01-08 11:55:11 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,13 +1,12 @@
 import streamlit as st
 st.get_page_config(page_title="Kings in flim Industry")
 st.header("Types of HEROS in Film industry")
-                   
-                   col1,col2=st.columns(2)
-                   with col1:
-                     st.subheader("Prabhas")
-                     st.image("prabhas.jpg",caption="Prabhas",width=300,use_column-width=True)
-                     st.write("Image from SALAAR")
-                   with col2:
-                     st.subheader("Yesh")
-                     st.image("Rocky.jpg",caption="YESH",width=300,use_column-width=True)
-                     st.write("Image from KGF")
+col1,col2=st.columns(2)
+with col1:
+  st.subheader("Prabhas")
+  st.image("prabhas.jpg",caption="Prabhas",width=300,use_column-width=True)
+  st.write("Image from SALAAR")
+with col2:
+  st.subheader("Yesh")
+  st.image("Rocky.jpg",caption="YESH",width=300,use_column-width=True)
+  st.write("Image from KGF")
