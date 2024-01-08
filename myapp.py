c1d6044f7046d2545642fb0b6ff774c87c6cda0c
pavan-tangella <136261705+pavan-tangella@users.noreply.github.com>
2024-01-08 12:01:26 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -4,9 +4,9 @@ st.header("HEROS in Film industry")
 col1,col2=st.columns(2)
 with col1:
   st.subheader("Prabhas")
-  st.image("prabhas.jpg",caption="Prabhas",width=300,height=300)
+  st.image("prabhas.jpg",caption="Prabhas",width=300)
   st.write("Image from SALAAR")
 with col2:
   st.subheader("Yesh")
-  st.image("Rocky.jpg",caption="YESH",width=300,height=300)
+  st.image("Rocky.jpg",caption="YESH",width=300)
   st.write("Image from KGF")
