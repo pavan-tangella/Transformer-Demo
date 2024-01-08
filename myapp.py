df79463315b9f2a7a60bf6d9f63612d5bfd863b8
pavan-tangella <136261705+pavan-tangella@users.noreply.github.com>
2024-01-08 12:11:06 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -2,11 +2,20 @@ import streamlit as st
 st.set_page_config(page_title="Bikes")
 st.header("JAWA vs RE")
 col1,col2=st.columns(2)
+col3,col4=stt.columns(2)
 with col1:
   st.subheader("JAWA PERAK")
   st.image("Jawa.jpeg",caption="JAWA PERAK",width=300)
   st.write("Jawa image")
 with col2:
-  st.subheader("Yesh")
+  st.subheader("Royal Enfiled")
+  st.image("RoyalEnfiled.jpeg",caption="ROYAL ENFILED 350",width=300)
+  st.write("RE image")
+with col3:
+  st.subheader("JAWA PERAK")
+  st.image("Jawa.jpeg",caption="JAWA PERAK",width=300)
+  st.write("Jawa image")
+with col4:
+  st.subheader("Royal Enfiled")
   st.image("RoyalEnfiled.jpeg",caption="ROYAL ENFILED 350",width=300)
   st.write("RE image")
