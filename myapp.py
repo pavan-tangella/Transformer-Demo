7de90a3a2c19b7713aa5f07d352838c9ce74ff0d
pavan-tangella <136261705+pavan-tangella@users.noreply.github.com>
2024-01-08 11:38:46 +0530

    Create myapp.py

---
+++ myapp.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.get_page_config(page_title="rocky bhais")
+st.header("Types of HEROS in Film industry")
+                   
+                   col1,col2=st.columns(2)
+                   with col1:
+                     st.subheader("Prabhas")
+                     st.image("")
+                     st.write("Image from SALAAR")
+                   with col2:
+                     st.subheader("Yesh")
+                     st.image("")
+                     st.write("Image from KGF")
+                     
